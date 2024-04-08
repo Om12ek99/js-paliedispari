8628999c09351feebd7433257ab7242500dba680
Om12ek99 <omar.elkamili@gmail.com>
2024-04-08 02:42:58 +0200

    add: js e richiamo alla funzione random

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,1 +1,11 @@
 // INPUT
+
+userNumbPod = prompt("pari o dispari (scrivi in minuscolo)");   //inserimento scelta
+console.log(userNumbPod);
+userNumb = parseInt(prompt("inserisci un numero intero da 1 a 5"));   //inserimento numero
+console.log(userNumb, "numero inserito");
+
+const randomNumb = randomNumber();                                    //funzione del numero random del pc
+
+const result = randomNumb + userNumb;
+console.log(result, "somma numeri");
